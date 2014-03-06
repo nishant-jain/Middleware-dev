b67e26384f11111a0d71397b90f8dbaaa13c939b
GarvitaBajaj <garvitab@192.168.16.113>
2014-03-07 01:27:08 +0530

    Individual sensor names extracted

---
+++ src/com/middleware/pubsubclient/PublishQuery.java
@@ -0,0 +1,69 @@
+package com.middleware.pubsubclient;
+
+import java.util.Date;
+
+import org.jivesoftware.smackx.pubsub.PubSubManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class PublishQuery extends Activity {
+
+	private Spinner deviceCount;
+	private EditText minCount;
+	private EditText max;
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_publish_query);
+		deviceCount = (Spinner)findViewById(R.id.Spinner02);
+		minCount = (EditText)findViewById(R.id.editText6);
+		max = (EditText)findViewById(R.id.EditText06);
+		deviceCount.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int pos, long arg3) {
+				if(pos==1)//Exact
+				{
+					minCount.setHint("Count");
+					max.setVisibility(View.INVISIBLE);
+				}
+				else//Range
+				{
+					minCount.setHint("Min");
+					max.setVisibility(View.VISIBLE);
+				}
+		}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+			
+		});
+	}
+	
+	public void onDeviceCountChange()
+	{
+		
+	}
+	
+	public void sendQuery(View v)
+	{
+		String sensorName="";
+		Date fromTime,toTime,expiryTime;
+		Double lat,lon;
+		String activity="";
+		int delay;
+		int gpsDelay;
+		int countMin,countMax;
+		
+		PubSubManager mgr=new PubSubManager(conn);
+	}
+	
+}
