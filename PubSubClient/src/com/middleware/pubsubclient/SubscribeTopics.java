9a82ef973baa1cc83fc43934f659ca4a4ae668cb
GarvitaBajaj <garvitab@192.168.16.113>
2014-03-24 17:16:24 +0530

    Updated code for registration and subscription

--- PubSubClient/src/com/middleware/pubsubclient/SubscribeTopics.java
+++ PubSubClient/src/com/middleware/pubsubclient/SubscribeTopics.java
@@ -1,7 +1,12 @@
 package com.middleware.pubsubclient;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.packet.Message;
 
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
@@ -11,51 +16,61 @@ import android.content.DialogInterface.OnClickListener;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.hardware.Sensor;
+import android.hardware.SensorListener;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
 public class SubscribeTopics extends PreferenceActivity{
 	
 	SharedPreferences preferences ;
+	String entries[];
+	StringBuilder list;
+	XMPPConnection conn;
 	
 	@SuppressWarnings("deprecation")
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // preferences=getSharedPreferences(RegisterMe.PREFS_NAME, 0);
         preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        conn=RegisterMe.conn;
         
         addPreferencesFromResource(R.xml.subscriptions);
         
         MultiSelectListPreference topicList = (MultiSelectListPreference) findPreference("sensorList");
         topicList.setPersistent(true);
         topicList.setEnabled(true);
-        if (topicList != null) {
-            List<Sensor> categoryList = RegisterMe.deviceSensors;
-            CharSequence entries[] = new String[categoryList.size()];
-            CharSequence entryValues[] = new String[categoryList.size()];
-            int i = 0;
-            for (Sensor category : categoryList) {
-                entries[i] = category.getName();
-                entryValues[i] = Integer.toString(i);
-                i++;
+        topicList.setDefaultValue(true);
+        if (topicList != null) 
+        {
+           	Iterator check=RegisterMe.obj.keys();
+            List<String> elements = new ArrayList<String>();
+           while(check.hasNext())
+           {
+        	   elements.add(check.next().toString());
+        	 }
+           
+            entries = new String[elements.size()-1];
+            String entryValues[] = new String[elements.size()-1];
+            int i=0;
+            for (String category : elements) {
+            	if(!category.equals("noSensors"))
+                {
+            		entries[i] = category;
+	                entryValues[i] = Integer.toString(i);
+	                i++;
+                }
             }
             topicList.setEntries(entries);
             topicList.setEntryValues(entryValues);
         }
+        
        
-	
-	
-    Set<String> selections = preferences.getStringSet("sensorList", null);
-    String[] selected= selections.toArray(new String[]{});
-     for(String s:selected)
-     {
-    	 System.out.println(s);
-     }
-	
 	
 	PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(new OnSharedPreferenceChangeListener() {
 			public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) 
@@ -71,12 +86,16 @@ public class SubscribeTopics extends PreferenceActivity{
 	public void updatePrefs()
 	{
 		System.out.println("Preferences changed....new preferences are");
-    	Set<String> selections = preferences.getStringSet("sensorList", null);
-        String[] selected= selections.toArray(new String[]{});
-        for(String s:selected)
-        {
-       	 System.out.println(s);
-        }
+		 Set<String> selections = preferences.getStringSet("sensorList", null);
+	        String[] selected = selections.toArray(new String[] {});
+	        list=new StringBuilder();
+		    for(String s:selected)
+		    {
+		    	list.append(entries[Integer.parseInt(s)]+"\n");		    	
+		    }
+		
+       // System.out.println(list.toString());
+        
         
     	@SuppressWarnings("deprecation")
 		AlertDialog.Builder alertb=new Builder(getPreferenceScreen().getContext());
@@ -98,9 +117,15 @@ public class SubscribeTopics extends PreferenceActivity{
 				
 				//add code to upload the preferences to the server
 				//send an xmpp iq messages to the server JID containing the updated list of sensors
-				if(isNetworkAvailable())
+				if(isNetworkAvailable() && conn.isConnected())
 				{
 					//send the list of sensors to the server
+					Message topics=new Message("server@103.25.231.23",Message.Type.normal);
+					topics.setSubject("Subscription");
+					topics.setBody(list.toString());
+					conn.sendPacket(topics);
+					System.out.println("Sent");
+					
 				}
 				else
 				{
